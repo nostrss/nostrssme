7daa3743ee61ecc2ee4665da2a3b90eb616a0e2a
nostrss <jintagi@gmail.com>
2025-05-02 20:20:27 +0900

    feat(blog): 저자 정보에서 트위터를 제거하고 github 링크로 변경

--- apps/blog/layouts/PostLayout.tsx
+++ apps/blog/layouts/PostLayout.tsx
@@ -74,16 +74,14 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                       <dl className="text-sm leading-5 font-medium whitespace-nowrap">
                         <dt className="sr-only">Name</dt>
                         <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
-                        <dt className="sr-only">Twitter</dt>
+                        <dt className="sr-only">Github</dt>
                         <dd>
-                          {author.twitter && (
+                          {author.github && (
                             <Link
-                              href={author.twitter}
+                              href={author.github}
                               className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
                             >
-                              {author.twitter
-                                .replace('https://twitter.com/', '@')
-                                .replace('https://x.com/', '@')}
+                              Github
                             </Link>
                           )}
                         </dd>
