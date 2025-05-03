841f5b25867b23c996fd4ae77cdc1bc8802657f4
nostrss <jintagi@gmail.com>
2025-05-03 13:53:27 +0900

    feat(blog): add logo image

--- apps/blog/components/Header.tsx
+++ apps/blog/components/Header.tsx
@@ -1,10 +1,11 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+import Logo from '@/data/logo.png'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
+import Image from 'next/image'
 
 const Header = () => {
   let headerClass = 'flex items-center w-full bg-white dark:bg-gray-950 justify-between py-10'
@@ -16,6 +17,7 @@ const Header = () => {
     <header className={headerClass}>
       <Link href="/" aria-label={siteMetadata.headerTitle}>
         <div className="flex items-center justify-between">
+          <Image className="mr-2 h-8 w-8 rounded-full" src={Logo} alt="logo" />
           {typeof siteMetadata.headerTitle === 'string' ? (
             <div className="block h-6 text-2xl font-semibold sm:block">
               {siteMetadata.headerTitle}
