a66bffc69df97af9d7d54c9166bbb514506ee915
nostrss <jintagi@gmail.com>
2026-02-20 23:56:41 +0900

    feat(blog): head에 Adsense Code 삽입

--- apps/blog/app/layout.tsx
+++ apps/blog/app/layout.tsx
@@ -11,6 +11,7 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
+import Script from 'next/script'
 
 const space_grotesk = Open_Sans({
   subsets: ['latin'],
@@ -94,6 +95,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4551649509417014"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
